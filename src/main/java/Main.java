f0552967cf61c04dd4ce7dd431488db04d183e5f
whatthefeng <aktanmuk04@gmail.com>
2025-04-21 20:44:05 -0500

    conflict 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Guys!" + "123123144");
         System.out.println("Hello !@!#!#@#@$");
-
     }
 }
