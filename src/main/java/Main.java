64ba15c32110ed24f536a20294c70766dff10992
Akamal Aralov <akmalaralov77@gmail.com>
2025-04-21 20:50:07 -0500

    changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Guys!");
+        System.out.println();
+        for (int l = 0; l < 1; l++) {
+
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
     }
 }
